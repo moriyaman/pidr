cd1b8548c7a9f90ddf4bf9d5b501ed08e07802ee
Yuki Moriyama <yuki.moriyaman@gmail.com>
2014-10-22 11:13:34 +0900

    first commit

---
+++ pidr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pidr
+//
+//  Created by Yuki Moriyama on 2014/10/22.
+//  Copyright (c) 2014年 yuki.moriyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
